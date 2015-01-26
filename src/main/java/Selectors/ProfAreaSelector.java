741d12870a549f32266135bcdc7998f6fb51cf40
Axelrod Vadim <axelrodvl@gmail.com>
2015-01-27 02:58:54 +0300

    Working backup 2.

---
+++ src/main/java/Selectors/ProfAreaSelector.java
@@ -0,0 +1,11 @@
+package Selectors;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProfAreaSelector {
+    private final WebDriver driver;
+
+    public ProfAreaSelector(WebDriver driver) {
+        this.driver = driver;
+    }
+}
--- src/main/java/Tabs/TabResumeBaseAccess.java
+++ src/main/java/Tabs/TabResumeBaseAccess.java
@@ -1,20 +1,39 @@
 package Tabs;
 
+import Selectors.ProfAreaSelector;
 import Selectors.RegionSelector;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class TabResumeBaseAccess {
     private final WebDriver driver;
+    private List<WebElement> offersList;
+    public int offersCount;
 
     public TabResumeBaseAccess(WebDriver driver) {
         this.driver = driver;
+        offersList = driver.findElements(offersLocator);
+        offersCount = offersList.size();
     }
 
     By regionsButtonSelector = By.xpath("//*[@id=\"js-disabled\"]/body/div[5]/div/div/div[2]/div[1]/div/ul/li[2]/div/div/div[3]/div[2]");
+    By profAreaButtonSelector = By.xpath("//*[@id=\"js-disabled\"]/body/div[5]/div/div/div[2]/div[1]/div/ul/li[2]/div/div/div[3]/div[2]");
+
+    By offersLocator = By.xpath("//*[@id=\"js-disabled\"]/body/div[5]/div/div/div[2]/div[1]/div/ul/li[2]/div/div/div[5]/div");
+    By addToBucketLocator = By.xpath("//*[@id=\"js-disabled\"]/body/div[5]/div/div/div[2]/div[1]/div/ul/li[2]/div/div/div[7]/div[2]/button");
 
     public RegionSelector regionSelector() {
         driver.findElement(regionsButtonSelector).click();
         return new RegionSelector(driver);
     }
+
+    public ProfAreaSelector profAreaSelector() {
+        driver.findElement(profAreaButtonSelector).click();
+        return new ProfAreaSelector(driver);
+    }
+
+
 }
