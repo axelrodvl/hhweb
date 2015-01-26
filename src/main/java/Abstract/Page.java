83b32b0c0499477583a012a3f76446a7905fa859
unknown <vakselrod@ws084.CINIMEX>
2015-01-26 18:56:36 +0300

    Initial commit

---
+++ src/main/java/Abstract/Page.java
@@ -0,0 +1,25 @@
+package Abstract;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class Page {
+    // ChromeDriver
+    private static final String CHROME_DRIVER_NAME = "chromedriver.exe";
+    private static final String CHROME_DRIVER_PROPERTY = "webdriver.chrome.driver";
+    protected static WebDriver driver = null;
+    // Page constants
+    protected static final String ROOT_URI = "http://hh.ru";
+
+    protected Page() {
+        System.setProperty(CHROME_DRIVER_PROPERTY, CHROME_DRIVER_NAME);
+        driver = new ChromeDriver();
+    }
+
+    public abstract void open();
+
+    public void close() {
+        driver.quit();
+    }
+}
+
---
+++ src/main/java/Abstract/TabsSwitcher.java
@@ -0,0 +1,16 @@
+package Abstract;
+
+import java.util.ArrayList;
+import org.openqa.selenium.WebElement;
+
+public abstract class TabsSwitcher {
+    private ArrayList<Form> tabs = new ArrayList<Form>();
+
+    protected TabsSwitcher(WebElement tabsForm) {}
+
+    private void createTab(WebElement tabForm) {
+        // MAGIC
+        Form form = new Form();
+        tabs.add(form);
+    }
+}
